b131918e867b5c58fae62e654397aee0a822e431
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-08-01 18:22:50 -0500

    blah blah blah

--- zoolanderapi/models/user.py
+++ zoolanderapi/models/user.py
@@ -2,5 +2,4 @@ from django.db import models
 
 class User(models.Model):
     """Model that represents a user"""
-    name = models.CharField(max_length=50)
     uid = models.CharField(max_length=50)
